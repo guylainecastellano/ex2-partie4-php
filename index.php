a5521f737708bfff5808c4f266edacb442c8dafe
guylainecastellano <guylaine.a@wanadoo.fr>
2020-05-11 11:48:39 +0200

    ex2 partie4 php

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+$phrase = 'chaine de caractère';
+$longueur = strlen($phrase);
+?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8"/>
+  <title>ex2 partie4 php</title>
+</head>
+<body>
+<?php
+echo $phrase .' '. $longueur;
+?>
+</body>
+</html>
